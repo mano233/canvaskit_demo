2ae589a938bebdb8a825ee454d424701e718fc29
mano233 <2360861135@qq.com>
2020-12-19 17:36:13 +0800

    multiply canvas

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <canvas id="canvas" style="border: 1px solid red;" height="800" width="800"></canvas>
+    <canvas id="canvas1" style="border: 1px solid green;" height="800" width="800"></canvas>
   </div>
 </template>
 
@@ -22,13 +23,15 @@ export default {
       return response.arrayBuffer()
     })
     CanvasKitInit().then((CanvasKit) => {
-      const surface = CanvasKit.MakeCanvasSurface(canvas.id);
+      const surface = CanvasKit.MakeWebGLCanvasSurface('canvas')
+      const surface1 = CanvasKit.MakeWebGLCanvasSurface('canvas1')
       if (!surface) {
         console.log('Could not make surface');
         return;
       }
-      const context = CanvasKit.currentContext();
-      const skcanvas = surface.getCanvas();
+      // const context = CanvasKit.currentContext();
+      // const skcanvas = surface.getCanvas();
+      // const skcanvas1 = surface1.getCanvas();
       // encode img
       let bg_img = CanvasKit.MakeImageFromEncoded(bg_img_data)
       let fr_img = CanvasKit.MakeImageFromEncoded(fr_img_data)
@@ -45,24 +48,29 @@ export default {
       // 保存一下原始的混合模式
       let baseBlendMode = paint.getBlendMode()
       let paint1 = paint.copy()
-      function drawFrame () {
-        CanvasKit.setCurrentContext(context);
-        skcanvas.drawImage(bg_img, 0, 0, paint)
-        let layer_0_id = skcanvas.saveLayer()
-        let layer_1_id = skcanvas.save()
+      function drawFrame (canvas) {
+        // CanvasKit.setCurrentContext(context);
+        canvas.drawImage(bg_img, 0, 0, paint)
+        let layer_0_id = canvas.saveLayer()
+        let layer_1_id = canvas.save()
 
-        skcanvas.scale(0.1, 0.1)
-        skcanvas.drawImage(fr_img, 0, 0, paint)
-        skcanvas.restoreToCount(layer_1_id)
+        canvas.scale(0.1, 0.1)
+        canvas.drawImage(fr_img, 0, 0, paint)
+        canvas.restoreToCount(layer_1_id)
         paint1.setColor(CanvasKit.Color(255, 0, 255, 1));
         // 绘制所有的笔刷
         for (let i = 0; i < paths.length; i++) {
           // 根据每个笔刷的混合模式来绘制（每次设置有性能损耗，应该可以继续优化，我笔记本风扇在咆哮）
           paint1.setBlendMode(paths[i].blend)
-          skcanvas.drawPath(paths[i].path, paint1)
+          canvas.drawPath(paths[i].path, paint1)
         }
-        skcanvas.restoreToCount(layer_0_id)
-        skcanvas.flush();
+        canvas.restoreToCount(layer_0_id)
+        canvas.flush();
+      }
+      function drawFrame1(canvas){
+        canvas.drawColor(CanvasKit.Color(255,255,0,1))
+        canvas.drawImage(CanvasKit.MakeImageFromCanvasImageSource(document.getElementById('canvas')),0,0,paint)
+        canvas.flush();
       }
 
       let hold = false;
@@ -79,14 +87,17 @@ export default {
           path.moveTo(e.offsetX, e.offsetY);
         }
         hold = true;
-        requestAnimationFrame(drawFrame);
+        surface.requestAnimationFrame(drawFrame);
+
       });
       canvas.addEventListener('mouseup',(e)=>{
         // 鼠标抬起的时候，修改最后一条笔刷的混合模式
         paths[paths.length-1].blend = CanvasKit.BlendMode.SrcATop
-        requestAnimationFrame(drawFrame);
+        surface.requestAnimationFrame(drawFrame);
+        surface1.requestAnimationFrame(drawFrame1)
       })
-      requestAnimationFrame(drawFrame);
+      surface.requestAnimationFrame(drawFrame);
+      surface1.requestAnimationFrame(drawFrame1)
     });
   }
 }
