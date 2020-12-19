ba82398f93c49f5910bc69eb5bef3612260bc948
mano233 <2360861135@qq.com>
2020-12-19 11:26:01 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <canvas id="canvas" style="border: 1px solid red;" height="400" width="400"></canvas>
+    <canvas id="canvas" style="border: 1px solid red;" height="800" width="800"></canvas>
   </div>
 </template>
 
@@ -15,8 +15,12 @@ export default {
   async mounted () {
     let canvas = document.getElementById('canvas')
     // let response = await fetch('https://img.iplaysoft.com/wp-content/uploads/2019/free-images/free_stock_photo.jpg')
-    let response = await  fetch('./UVMap.png')
-    let img_data = await response.arrayBuffer()
+    let bg_img_data = await fetch('./static/bg.jpg').then((response) => {
+      return response.arrayBuffer()
+    })
+    let fr_img_data = await fetch('./static/fr.png').then((response) => {
+      return response.arrayBuffer()
+    })
     CanvasKitInit().then((CanvasKit) => {
       const surface = CanvasKit.MakeCanvasSurface(canvas.id);
       if (!surface) {
@@ -25,24 +29,43 @@ export default {
       }
       const context = CanvasKit.currentContext();
       const skcanvas = surface.getCanvas();
-      let img = CanvasKit.MakeImageFromEncoded(img_data)
-      let paint = new CanvasKit.Paint();
-      paint.setAntiAlias(true);
-      paint.setColor(CanvasKit.Color(0, 0, 0, 1.0));
-      paint.setStyle(CanvasKit.PaintStyle.Stroke);
-      paint.setStrokeWidth(4.0);
-      paint.setPathEffect(CanvasKit.PathEffect.MakeCorner(50));
+      // encode img
+      let bg_img = CanvasKit.MakeImageFromEncoded(bg_img_data)
+      let fr_img = CanvasKit.MakeImageFromEncoded(fr_img_data)
+      let paint = new CanvasKit.Paint()
+      paint.setAntiAlias(true)
+      paint.setColor(CanvasKit.Color(0, 0, 0, 1.0))
+      paint.setStyle(CanvasKit.PaintStyle.Stroke)
+      paint.setStrokeWidth(4.0)
+      paint.setPathEffect(CanvasKit.PathEffect.MakeCorner(50))
 
       let path = new CanvasKit.Path();
-      let paths = [path];
-      let paints = [paint];
+      let paths = [];
+
+      // 保存一下原始的混合模式
+      let baseBlendMode = paint.getBlendMode()
+
+      // 混合模式变量
+      let blendMode = baseBlendMode
 
       function drawFrame () {
         CanvasKit.setCurrentContext(context);
-        skcanvas.drawImage(img, 128, 128, paints[0]);
-        for (let i = 0; i < paints.length && i < paths.length; i++) {
-          skcanvas.drawPath(paths[i], paints[i]);
+        skcanvas.drawImage(bg_img, 0, 0, paint)
+        let layer_0_id = skcanvas.saveLayer()
+        let layer_1_id = skcanvas.save()
+
+        skcanvas.scale(0.1, 0.1)
+        skcanvas.drawImage(fr_img, 0, 0, paint)
+        skcanvas.restoreToCount(layer_1_id)
+        let paint1 = paint.copy()
+        paint1.setColor(CanvasKit.Color(255, 0, 255, 1));
+        // 绘制所有的笔刷
+        for (let i = 0; i < paths.length; i++) {
+          // 根据每个笔刷的混合模式来绘制（每次设置有性能损耗，应该可以继续优化，我笔记本风扇在咆哮）
+          paint1.setBlendMode(paths[i].blend)
+          skcanvas.drawPath(paths[i].path, paint1)
         }
+        skcanvas.restoreToCount(layer_0_id)
         skcanvas.flush();
         requestAnimationFrame(drawFrame);
       }
@@ -50,24 +73,23 @@ export default {
       let hold = false;
       canvas.addEventListener('mousemove', (e) => {
         if (!e.buttons) {
-          hold = false;
-          return;
+          hold = false
+          return
         }
         if (hold) {
           path.lineTo(e.offsetX, e.offsetY);
         } else {
-          paint = paint.copy();
-          paint.setColor(
-            CanvasKit.Color(Math.random() * 255, Math.random() * 255, Math.random() * 255, Math.random() + .2));
-          paints.push(paint);
           path = new CanvasKit.Path();
-          paths.push(path);
+          paths.push({path,blend:baseBlendMode});
           path.moveTo(e.offsetX, e.offsetY);
         }
         hold = true;
       });
+      canvas.addEventListener('mouseup',(e)=>{
+        // 鼠标抬起的时候，修改最后一条笔刷的混合模式
+        paths[paths.length-1].blend = CanvasKit.BlendMode.SrcATop
+      })
       requestAnimationFrame(drawFrame);
-
     });
   }
 }
